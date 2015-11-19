d56876eaa1d2c00e04821ff243b9dc72ba212248
Nicola <nicola.amatucci@gmail.com>
2015-11-20 00:27:46 +0100

    Initial Commit

---
+++ GDriveForceUpdateContext.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GDriveForceUpdate
+{
+    public class GDriveForceUpdateContext : ApplicationContext
+    {
+        public static Boolean running = false;
+        public static Boolean paused = false;
+
+        private System.ComponentModel.IContainer mComponents;
+        private NotifyIcon mNotifyIcon;
+        private ContextMenuStrip mContextMenu;
+        private ToolStripMenuItem mDisplayForm;
+        private ToolStripMenuItem pauseUpdate;
+        private ToolStripMenuItem mExitApplication;
+
+        BackgroundWorker m_oWorker;
+
+        public GDriveForceUpdateContext()
+        {
+            running = true;
+
+            mComponents = new System.ComponentModel.Container();
+
+            mNotifyIcon = new NotifyIcon(this.mComponents);
+            //System.Reflection.Assembly myAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+            //Stream myStream = myAssembly.GetManifestResourceStream("icon.png");
+            Stream s = GetType().Assembly.GetManifestResourceStream("GDriveForceUpdate.icon.png");
+            System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(s);
+            s.Close();
+            mNotifyIcon.Icon = System.Drawing.Icon.FromHandle(bmp.GetHicon());
+            mNotifyIcon.Text = "GDrive Force Update";
+            mNotifyIcon.Visible = true;
+
+            mContextMenu = new ContextMenuStrip();
+            mDisplayForm = new ToolStripMenuItem();
+            mExitApplication = new ToolStripMenuItem();
+            pauseUpdate = new ToolStripMenuItem();
+
+            mNotifyIcon.ContextMenuStrip = mContextMenu;
+
+            mDisplayForm.Text = "About...";
+            mDisplayForm.Click += new EventHandler(mDisplayForm_Click);
+            mContextMenu.Items.Add(mDisplayForm);
+
+            pauseUpdate.Text = "Pause";
+            pauseUpdate.Click += new EventHandler(mPauseUpdate_Click);
+            mContextMenu.Items.Add(pauseUpdate);
+
+            mExitApplication.Text = "Exit";
+            mExitApplication.Click += new EventHandler(mExitApplication_Click);
+            mContextMenu.Items.Add(mExitApplication);
+
+            
+
+            m_oWorker = new BackgroundWorker();
+            m_oWorker.DoWork += new DoWorkEventHandler(m_oWorker_DoWork);
+            //m_oWorker.ProgressChanged += new ProgressChangedEventHandler(m_oWorker_ProgressChanged);
+            //m_oWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(m_oWorker_RunWorkerCompleted);
+            m_oWorker.WorkerReportsProgress = true;
+            m_oWorker.WorkerSupportsCancellation = true;
+            m_oWorker.RunWorkerAsync();
+        }
+
+        void m_oWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            String taskKillCmd = "taskkill";
+            String taskKillArgs = "/F /IM googledrivesync.exe";
+            String taskStartCmd = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) + @"\Google\Drive\googledrivesync.exe";
+
+            Process taskKillProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = taskKillCmd,
+                    Arguments = taskKillArgs,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    CreateNoWindow = true,
+                    Verb = "runas"
+                }
+            };
+
+
+            Process taskStartProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = taskStartCmd,
+                    //Arguments = "",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    //WindowStyle = ProcessWindowStyle.Hidden,
+                    CreateNoWindow = true,
+                    WorkingDirectory = @"C:\Program Files (x86)\Google\Drive\",
+                    Verb = "runas"
+                }
+            };
+
+            do
+            {
+                if (paused == false)
+                {
+                    taskKillProcess.Start();
+                    taskKillProcess.WaitForExit();
+
+                    taskStartProcess.Start();
+                }
+
+                Thread.Sleep(3 * 60 * 1000);
+
+            } while (running) ;
+        }
+
+        void mDisplayForm_Click(object sender, EventArgs e)
+        {
+            new GDriveForceUpdateForm().Show();
+        }
+
+        void mPauseUpdate_Click(object sender, EventArgs e)
+        {
+            if (paused == true)
+            {
+                pauseUpdate.Text = "Pause";
+                paused = false;
+            }
+            else
+            {
+                pauseUpdate.Text = "Activate";
+                paused = true;
+            }
+            
+        }
+
+        void mExitApplication_Click(object sender, EventArgs e)
+        {
+            running = false;
+            ExitThreadCore();
+        }
+
+        protected override void ExitThreadCore()
+        {
+            base.ExitThreadCore();
+        }
+    }
+}
---
+++ GDriveForceUpdateForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace GDriveForceUpdate
+{
+    partial class GDriveForceUpdateForm
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GDriveForceUpdateForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 78);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // GDriveForceUpdateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 143);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GDriveForceUpdateForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GDrive Force Update";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
